9bb954c80943377ea431c87eb3d0166f3fe52c5a
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-04-17 17:28:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
-var playlist = {[playlist]: 'i love you'};
+var playlist = {[playlist]: artist};
 
 function updatePlaylist(playlist, artistName, songTitle){
 playlist['Phil Ochs'] = 'Slowdive';
 return playlist;
 }
 
-function removeFromPlaylist(playlist, artistName){
-delete playlist.artistName 
-}
+// function removeFromPlaylist(playlist, artistName){
+// delete playlist.artistName 
+// }
